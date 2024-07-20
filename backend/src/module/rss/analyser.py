d686f363efae167f60c845db405645d046fc8961
limil <gravitylmlml@gmail.com>
2024-07-20 23:34:41 +0800

    可以自定义番剧名

--- backend/src/module/rss/analyser.py
+++ backend/src/module/rss/analyser.py
@@ -22,8 +22,11 @@ class RSSAnalyser(TitleParser):
                 logger.warning("[Parser] Mikan torrent has no homepage info.")
                 pass
         elif rss.parser == "tmdb":
+            name = bangumi.official_title
+            if not rss.aggregate and rss.name:
+                name = rss.name
             tmdb_title, season, year, poster_link = self.tmdb_parser(
-                bangumi.official_title, bangumi.season, settings.rss_parser.language
+                name , bangumi.season, settings.rss_parser.language
             )
             bangumi.official_title = tmdb_title
             bangumi.year = year
